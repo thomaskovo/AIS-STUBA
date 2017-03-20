68f1509e66ea7880369506bdfa078828430acccd
Tomáš Kováčik <tomasko.kovacik@gmail.com>
2017-03-20 14:27:17 +0100

    pointer

--- js.js
+++ js.js
@@ -70,15 +70,13 @@ $("a[href='nova_zprava.pl']").parent().parent().click(function(){
    if($('#newMessage').attr('class')=='close'){
 $('#newMessage').addClass('open').removeClass('close');
 $('#NM').addClass('NMopen').removeClass('NMclose');
+       $('body').css({'pointer-events:none'});
 }
 else {$('#newMessage').addClass('close').removeClass('open');
 $('#NM').addClass('NMclose').removeClass('NMopen');
+      $('body').css({'pointer-events:all'});
       
-      
-      setTimeout(function() {
-  /*$("a[href='nova_zprava.pl']").parent().parent();*/
-          console.log('leave')
-}, 1000);
+     
       
 } 
     
